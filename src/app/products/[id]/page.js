6ad2f967be365208123a24fd87782d1d8a89fa95
RonyBoom <ronylevi99@gmail.com>
2025-03-18 18:26:20 +0200

    sync

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 import { useSetAtom } from 'jotai';
 import { useState, useEffect } from 'react';
 import { useParams } from "next/navigation";
-import cartAtom from '../../store/CartAtom';
+import { cartAtom } from '../../store/CartAtom';
 
 
 export default function ProductDetails() {
